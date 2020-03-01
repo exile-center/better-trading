72d0856c9a023b55f6c96baa864c5423b0af617b
Pascal Boutin <pboutin@mirego.com>
2020-03-01 16:46:29 -0500

    Introduce autofocus for bookmarks forms
    Also bind the Escape key to close modals
    
    resolves https://github.com/exile-center/better-trading/issues/8

--- app/pods/components/modal/component.ts
+++ app/pods/components/modal/component.ts
@@ -1,9 +1,26 @@
 // Vendor
 import Component from '@glimmer/component';
+import {action} from '@ember/object';
 
 interface Args {
   title: string;
   onClose: () => {};
 }
 
-export default class Modal extends Component<Args> {}
+export default class Modal extends Component<Args> {
+  @action
+  bindEscapeKey() {
+    document.body.addEventListener('keyup', this.handleKeyup.bind(this));
+  }
+
+  @action
+  unbindEscapeKey() {
+    document.body.removeEventListener('keyup', this.handleKeyup.bind(this));
+  }
+
+  private handleKeyup(event: KeyboardEvent) {
+    if (event.key !== 'Escape') return;
+
+    this.args.onClose();
+  }
+}
