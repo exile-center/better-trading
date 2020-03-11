bdcd03d5ec2e7a9ea0b34e24891c8e154a97849c
Pascal Boutin <pboutin@mirego.com>
2020-03-10 21:53:36 -0400

    Display the version number in the about page

--- app/pods/components/page/about/component.ts
+++ app/pods/components/page/about/component.ts
@@ -5,6 +5,7 @@ import Component from '@glimmer/component';
 import config from 'better-trading/config/environment';
 
 export default class PageAbout extends Component {
+  appVersion = config.APP.version;
   githubUrl = config.APP.githubUrl;
   discordUrl = config.APP.discordUrl;
   chaosRecipeOverlayUrl = config.APP.chaosRecipeOverlayUrl;
