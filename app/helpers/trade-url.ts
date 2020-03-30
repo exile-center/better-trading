eca9e55bc0daf237b2439bfb568559c0953e1948
Pascal Boutin <pboutin@mirego.com>
2020-03-30 14:16:59 -0400

    Hotfix for the trade URLs

--- app/helpers/trade-url.ts
+++ app/helpers/trade-url.ts
@@ -1,13 +1,37 @@
 // Vendor
 import Helper from '@ember/component/helper';
 import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency-decorators';
+import {timeout} from 'ember-concurrency';
 
 // Types
 import Location from 'better-trading/services/location';
+import performTask from 'better-trading/utilities/perform-task';
+
+// Constants
+const URL_REFRESH_DELAY_IN_MILLISECONDS = 1000;
 
 export default class TradeUrl extends Helper {
   @service('location')
   location: Location;
+
+  @task
+  *urlUpdateTask() {
+    yield timeout(URL_REFRESH_DELAY_IN_MILLISECONDS);
+
+    if (document.hasFocus()) {
+      this.recompute();
+    }
+
+    performTask(this.urlUpdateTask);
+  }
+
+  init() {
+    super.init();
+
+    // TODO: maybe rework with better url tracking
+    performTask(this.urlUpdateTask);
+  }
 
   compute(params: string[]) {
     const [type, slug, suffix] = params;
