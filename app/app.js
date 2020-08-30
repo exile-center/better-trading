e0f689203638473333159622704e755c75bc05e3
Pascal Boutin <contact@pboutin.net>
2020-08-30 16:30:59 -0400

    Avoid showing up the extension when the trade site is down (#65)
    github: #51

--- app/app.js
+++ app/app.js
@@ -4,11 +4,19 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 // Initialize the extension root container
-const isCollapsed = Boolean(window.localStorage.getItem('bt-side-panel-collapsed'));
 const extensionContainer = document.createElement('div');
 extensionContainer.id = 'better-trading-container';
-document.body.classList.add('bt-body');
-if (isCollapsed) document.body.classList.add('bt-is-collapsed');
+
+// Check if the trading app is present (ie. not in maintenance)
+if (document.querySelector('#trade')) {
+  document.body.classList.add('bt-body');
+
+  const isCollapsed = Boolean(window.localStorage.getItem('bt-side-panel-collapsed'));
+  if (isCollapsed) document.body.classList.add('bt-is-collapsed');
+} else {
+  extensionContainer.style.display = 'none';
+}
+
 document.body.appendChild(extensionContainer);
 
 const {modulePrefix, podModulePrefix} = config;
