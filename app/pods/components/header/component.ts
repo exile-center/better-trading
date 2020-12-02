04b53b26dc0ff7de4e34faba0bec4080a0a62e70
Pascal Boutin <pboutin@mirego.com>
2020-12-02 18:26:03 -0500

    Rework the about page

--- app/pods/components/header/component.ts
+++ app/pods/components/header/component.ts
@@ -3,6 +3,7 @@ import {inject as service} from '@ember/service';
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
+import RouterService from '@ember/routing/router-service';
 
 // Types
 import Storage from 'better-trading/services/storage';
@@ -11,11 +12,18 @@ import Storage from 'better-trading/services/storage';
 const COLLAPSED_STORAGE_KEY = 'side-panel-collapsed';
 
 export default class Header extends Component {
+  @service('router')
+  router: RouterService;
+
   @service('storage')
   storage: Storage;
 
   @tracked
   expandButtonIsVisible: boolean = Boolean(this.storage.getLocalValue(COLLAPSED_STORAGE_KEY));
+
+  get isOnRootRoute() {
+    return this.router.currentRouteName === 'root';
+  }
 
   @action
   collapse() {
