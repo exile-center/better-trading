ab88186b179636365320e30afc70b13ba3eb991c
Dan Bjorge <dan@dbjorge.net>
2024-12-13 18:45:47 -0800

    fix: don't attempt equivalent pricing enhancer in poe2

--- app/services/item-results/enhancers/equivalent-pricings.ts
+++ app/services/item-results/enhancers/equivalent-pricings.ts
@@ -29,7 +29,9 @@ export default class EquivalentPricings extends Service implements ItemResultsEn
 
   async prepare() {
     const currentLeague = this.tradeLocation.league;
-    this.chaosRatios = currentLeague ? await this.poeNinja.fetchChaosRatiosFor(currentLeague) : null;
+    const supportedVersion = this.tradeLocation.version === '1';
+    const supported = supportedVersion && currentLeague;
+    this.chaosRatios = supported ? await this.poeNinja.fetchChaosRatiosFor(currentLeague) : null;
   }
 
   // eslint-disable-next-line complexity
